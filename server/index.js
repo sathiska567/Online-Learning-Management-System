fbd2b4295f6b2d5091144617dcccc4e419dd58df
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2025-01-12 08:36:20 +0530

    created

---
+++ server/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+// const connectDB = require('./db/dbConfig'); 
+
+// const authRoute = require('./routes/authRoute');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Start the server
+const PORT = 8080;
+
+// Connect to the database
+// connectDB();
+
+
+// app.use("/api/v1/auth", authRoute);
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
