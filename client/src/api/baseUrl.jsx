dae660b9b9f67649e897c86c84ccb07bf879cd88
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2025-01-21 23:46:15 +0530

    add

--- client/src/api/baseUrl.jsx
+++ client/src/api/baseUrl.jsx
@@ -3,7 +3,7 @@ import { message } from 'antd';
 
 const api = axios.create({
 
-  baseURL: 'http://13.60.87.236:8080/api/v1',
+  baseURL: 'http://localhost:8080/api/v1',
 
   headers: {
     'Content-Type': 'application/json',
