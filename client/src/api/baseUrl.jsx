761c1018f8e781ea536a55bde142221d40b01796
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2025-01-22 13:03:40 +0530

    port change

--- client/src/api/baseUrl.jsx
+++ client/src/api/baseUrl.jsx
@@ -3,7 +3,7 @@ import { message } from 'antd';
 
 const api = axios.create({
 
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: 'http://13.49.68.136:8080/api/v1',
 
   headers: {
     'Content-Type': 'application/json',
